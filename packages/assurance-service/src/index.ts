8f50f864ac875fb681c40ae575088022eebf7255
Henry Tyler Hebert <henrytylerhebert@gmail.com>
2026-09-13 18:35:51 -0500

    feat(sr02): export surveillance services

--- packages/assurance-service/src/index.ts
+++ packages/assurance-service/src/index.ts
@@ -8,5 +8,9 @@ export {
 } from "./errors.js";
 export * from "./permissions.js";
 export * from "./evaluator.js";
+export * from "./surveillanceRuleEngine.js";
+export * from "./surveillanceEvaluator.js";
+export * from "./medicationRoomBlueprint.js";
+export * from "./surveillanceService.js";
 export { AssuranceCommandService } from "./assuranceCommandService.js";
 export { AssuranceQueryService } from "./assuranceQueryService.js";
