94d57dff58e7a370ba9bf17972502eb7c6de3049
Henry Tyler Hebert <henrytylerhebert@gmail.com>
2026-09-12 21:11:59 -0500

    feat(oa): export assurance service commands and queries

--- packages/assurance-service/src/index.ts
+++ packages/assurance-service/src/index.ts
@@ -1,1 +1,6 @@
+export * from "./commands.js";
+export * from "./errors.js";
+export * from "./permissions.js";
 export * from "./evaluator.js";
+export * from "./assuranceCommandService.js";
+export * from "./assuranceQueryService.js";
